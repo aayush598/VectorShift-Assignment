cc4543d6c19657a4eee903d1cd4fb6ee067f1f9f
aayush598 <aayushgid598@gmail.com>
2026-01-04 14:59:44 +0530

    backend url env name updated

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 // frontend/src/config/api.js
 
 const LOCAL_API = "http://127.0.0.1:8000";
-const PROD_API = process.env.API_BACKEND_URL;
+const PROD_API = process.env.REACT_APP_BACKEND_URL;
 
 export const API_BASE_URL = PROD_API || LOCAL_API;
