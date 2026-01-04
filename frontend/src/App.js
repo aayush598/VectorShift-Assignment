6042233178d5a495603c9d5906a3602a8d080540
aayush598 <aayushgid598@gmail.com>
2026-01-04 13:18:17 +0530

    toast fixed now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { PipelineToolbar } from "./toolbar";
 import { PipelineUI } from "./ui";
 import { SubmitButton } from "./submit";
+import { Bounce, ToastContainer } from "react-toastify";
 
 function App() {
   return (
@@ -20,6 +21,20 @@ function App() {
           <SubmitButton />
         </div>
       </div>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
     </div>
   );
 }
